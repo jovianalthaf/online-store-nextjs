507398fb54c984c23a674eb410d1a546903ccab6
jovianalthaf <mpialthaf@yahoo.co.id>
2025-06-29 02:30:45 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            💕
+            💕🚀
           </a>
         </div>
       </main>
